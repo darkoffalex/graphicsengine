81175123e478f9bfbfc7c7303fec37270b6031de
Alex New <darkoffalex@yandex.ru>
2018-11-25 00:39:29 +0200

    Сверточное ядро для пост-обработки (блур)

--- Shaders/post-processing.glsl
+++ Shaders/post-processing.glsl
@@ -29,11 +29,40 @@ out vec4 color;
 // Текстура экрана
 uniform sampler2D screenTexture;
 
+// Сдвиг текстурных координат для выборки соседних текселей
+const float offset = 0.003;  
+
 void main()
 {
-	// Черно-белый фильтр
-	vec4 FragColor = texture(screenTexture, vsoUV);
-	float average = (FragColor.r + FragColor.g + FragColor.b) / 3.0;
-	color = vec4(average, average, average, 1.0);
+	// Массив сдвигов UV координат для получения соседних текселей текстуры
+	vec2 offsets[9] = vec2[](
+		vec2(-offset, offset), vec2( 0.0f, offset), vec2( offset, offset),
+		vec2(-offset, 0.0f), vec2( 0.0f, 0.0f), vec2( offset, 0.0f),
+		vec2(-offset, -offset), vec2( 0.0f, -offset), vec2( offset, -offset)
+	);
+
+	// Коэфициенты умножения (сверточное ядро)
+	// Без изменений (исходная картинка)
+    //float kernel[9] = float[](
+    //    0, 0, 0,
+    //    0, 1, 0,
+    //    0, 0, 0
+    //);
+
+	// Размытие
+	float kernel[9] = float[](
+		1.0 / 16, 2.0 / 16, 1.0 / 16,
+		2.0 / 16, 4.0 / 16, 2.0 / 16,
+		1.0 / 16, 2.0 / 16, 1.0 / 16  
+    );
+
+	vec3 result;
+	for(int i = 0; i < 9; i++)
+	{
+		// Умножаем значение из ядра на значение соседнего текселя и прибавляем к результату
+		result += vec3(texture(screenTexture, vsoUV.xy + offsets[i])) * kernel[i];
+	}
+
+	color = vec4(result, 1.0);
 }
 /*FRAGMENT-SHADER-END*/
